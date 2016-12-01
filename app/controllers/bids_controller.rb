f888c9ffac258cb1ad30f428e85f779c48c86c3d
Vader0 <vader0@protonmail.com>
2016-12-01 17:47:09 +0100

    functional bids

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,7 +3,10 @@ class BidsController < ApplicationController
   end
 
   def create
-    @bid = Bid.new
+    @user_bid = User.find_by(email: params[:user_bid][:email])
+    @product_bid =Product.find(params[:id])
+    @bid = Bid.create(amount: params[:user_bid][:amount], user: @user_bid, product: @product_bid)
+    redirect_to controller: 'products', action: 'show_product'
   end
 
   def destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create([
-  {name: 'Yoda', email: 'yoda@lightside.com'},
-  {name: 'Vader', email: 'vader@darkside.com'}
-  ])
+  {name: 'Yoda',     email: 'yoda@lightside.com'},
+  {name: 'Vader',    email: 'vader@darkside.com'},
+  {name: 'Han Solo', email: 'hs@smuggler.com'}
+])
+
+Product.create([
+  {title: 'Spare Green Lightsaber', description: 'It will kill all motherfucker Siths',      deadline: '2016-12-31 20:30:00', user_id: 1, min_bid: 250.5},
+  {title: 'Spare Jedi Robes',       description: 'Nice Jedi clothing',                       deadline: '2016-12-31 20:30:00', user_id: 1, min_bid: 100},
+  {title: 'Spare Red Lightsaber',   description: 'It will kill all motherfucker weak Jedis', deadline: '2016-12-31 20:30:00', user_id: 2, min_bid: 250.5},
+  {title: 'Spare Sith Robes',       description: 'Nice Sith clothing',                       deadline: '2016-12-31 20:30:00', user_id: 2, min_bid: 100},
+  {title: 'Millennium falcon',      description: 'A legendary starship',                     deadline: '2016-12-31 20:30:00', user_id: 3, min_bid: 1000.5},
+  {title: 'Chewbacca',              description: 'A legendary Wookiee warrior',              deadline: '2016-12-31 20:30:00', user_id: 3, min_bid: 250}
+])
